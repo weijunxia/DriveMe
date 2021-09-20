f24a045cee87be39412ca113f25331d308afa894
mhydara0624 <mhydara0624@icloud.com>
2021-09-20 13:27:13 -0400

    changed script

--- seeders/20210920171655-user.js
+++ seeders/20210920171655-user.js
@@ -1,24 +1,25 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return queryInterface.bulkInsert(
+      'teams',
+      [
+        {},
+        {
+          name: 'WIND',
+          territory: 'Land of Blue Skies'
+        },
+        {
+          name: 'WAVE',
+          territory: 'Sea Towns'
+        }
+      ],
+      {}
+    )
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('teams')
   }
-};
+}
