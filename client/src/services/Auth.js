ef9e1d98a0a17629300c030a76046abf320a26bc
Derek <dvalenciano1986@gmail.com>
2021-09-20 15:35:42 -0500

    Created ProtectedRoute component, updated to remove comented out code

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -3,7 +3,6 @@ import Client from './api'
 export const SignInUser = async (data) => {
   try {
     const res = await Client.post('/auth/login', data)
-    // Set the current signed in users token to localstorage
     localStorage.setItem('token', res.data.token)
     return res.data.user
   } catch (error) {
@@ -22,7 +21,6 @@ export const RegisterUser = async (data) => {
 
 export const CheckSession = async () => {
   try {
-    // Checks if the current token if it exists is valid
     const res = await Client.get('/auth/session')
     return res.data
   } catch (error) {
