eb0f359cd6b8f23f98b510c6be019c582bfbb9af
SvetLana203 <shirapovauu@gmail.com>
2021-09-20 12:23:36 -0500

    cards,searchRes

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,9 +1,42 @@
 import React from 'react'
-
-function SearchResults() {
+import './SearchResult.css'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import StarIcon from '@material-ui/icons/Star'
+function SearchResults({
+  car_pic,
+  make,
+  model,
+  year,
+  description,
+  star,
+  price,
+  total
+}) {
   return (
     <div className="searchResults">
-      <div></div>
+      <img src={car_pic} alt="" />
+      <FavoriteBorderIcon className="searchResults_heart" />
+      <div className="searchResults_info">
+        <div className="searchResults_topInfo">
+          <h3>{make}</h3>
+          <h3>{model}</h3>
+          <p>{year}</p>
+          <p>______</p>
+          <p>{description}</p>
+        </div>
+        <div className="searchResults_bottomInfo">
+          <div className="searchResults_stars">
+            <StarIcon className="searchResults_star" />
+            <p>
+              <strong>{star}</strong>
+            </p>
+          </div>
+          <div className="searchResults_price">
+            <h2>{price}</h2>
+            <p>{total}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
