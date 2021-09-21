01b75488aa53badbc6345b65e4e4cb73b1da549c
Derek <dvalenciano1986@gmail.com>
2021-09-21 12:02:43 -0500

    updated NavBar css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,25 +2,26 @@ import React from 'react'
 import './navbar.css'
 import SearchIcon from '@material-ui/icons/Search'
 import logo from '../images/DriveMe.png'
-import LanguageIcon from '@material-ui/icons/Language'
+// import LanguageIcon from '@material-ui/icons/Language'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import { Avatar } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
   return (
     <div className="navBar">
-      <div className="navBar_search">
+      <Link to="/">
         <img src={logo} alt="DriveMe logo" className="navBar_logo" />
-        <div className="navBar_center">
-          <input type="text" />
-          <SearchIcon />
-        </div>
-        <div className="navBar_right">
-          <p>Become a host</p>
-          <LanguageIcon />
-          <ExpandMoreIcon />
-          <Avatar />
-        </div>
+      </Link>
+      <div className="navBar_center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="navBar_right">
+        <p>Become a host</p>
+        {/* <LanguageIcon /> */}
+        <ExpandMoreIcon />
+        <Avatar />
       </div>
     </div>
   )
