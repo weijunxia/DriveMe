03d4765756e2062d132680435db3975ee2f6293e
Wei Jun Xia <weijunx1224@gmail.com>
2021-09-21 10:09:33 -0400

    hello world

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
+import Cards from './components/Cards'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <Router>
           <NavBar />
+          <Cards />
           <Switch></Switch>
           <Footer />
         </Router>
