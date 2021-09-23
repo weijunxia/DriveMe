e662646c5804a14dee85c9947ca2f06b2ac81e7e
Wei Jun Xia <weijunx1224@gmail.com>
2021-09-23 14:33:53 -0400

    search results

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import './searchResults.css'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 import StarIcon from '@material-ui/icons/Star'
+import { NavLink } from 'react-router-dom'
 import { GetCars } from '../services/CarServices'
 
 function SearchResults(props) {
@@ -9,35 +10,37 @@ function SearchResults(props) {
     <div className="search_results">
       <div className="search_results_card">
         {props.cars.map((car) => (
-          <div className="search_result_cars" key={car.id}>
-            <img
-              src={car.car_pic}
-              alt={`${car.make} ${car.model} ${car.year}`}
-            />
-            <FavoriteBorderIcon className="search_result_heart" />
+          <NavLink to={`/cars/${car.id}`} style={{ textDecoration: 'none' }}>
+            <div className="search_result_cars" key={car.id}>
+              <img
+                src={car.car_pic}
+                alt={`${car.make} ${car.model} ${car.year}`}
+              />
+              <FavoriteBorderIcon className="search_result_heart" />
 
-            <div className="search_result_info">
-              <div className="search_result_top_info">
-                <h3>{car.make}</h3>
-                <h3>{car.model}</h3>
-                <h3>{car.year}</h3>
-                <p>{car.description}</p>
-              </div>
-
-              <div className="search_result_bottom_info">
-                <div className="search_result_stars">
-                  <StarIcon className="search_result_star" />
-                  <p>
-                    <strong>Stars</strong>
-                  </p>
+              <div className="search_result_info">
+                <div className="search_result_top_info">
+                  <h3>{car.make}</h3>
+                  <h3>{car.model}</h3>
+                  <h3>{car.year}</h3>
+                  <p>{car.description}</p>
                 </div>
 
-                <div className="search_result_price">
-                  <h2>${car.price}/day</h2>
+                <div className="search_result_bottom_info">
+                  <div className="search_result_stars">
+                    <StarIcon className="search_result_star" />
+                    <p>
+                      <strong>Stars</strong>
+                    </p>
+                  </div>
+
+                  <div className="search_result_price">
+                    <h2>${car.price}/day</h2>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </NavLink>
         ))}
       </div>
     </div>
